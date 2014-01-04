372baaf2cd4c6b860bfcfd1f0949bf90990020a7
Cheng Lou <chenglou92@gmail.com>
2014-01-04 07:02:19 -0800

    update tests fixtures

---
+++ tests/statics.spec.js
@@ -0,0 +1,33 @@
+describe('statics', function() {
+  it('should work', function() {
+    var collectStatic = require('../src/');
+    var fs = require('fs');
+    var path = require('path');
+
+    var done = false;
+
+    var fileNames = [
+      'react-components_spinner.css',
+      'react-table_table.css',
+      'react-spinner2_spinner.css',
+      'react-treeview_treeview.css',
+      'react-spinner2_spinner.png',
+      'react-treeview_treeview.png'
+    ];
+
+    runs(function() {
+      expect(function() {
+        collectStatic(__dirname, function() {
+          fileNames.forEach(function(fileName) {
+            expect(fs.existsSync(path.join(__dirname, 'build/static', fileName))).toBe(true);
+          });
+          done = true;
+        });
+      }).not.toThrow();
+    });
+
+    waitsFor(function() {
+      return done;
+    });
+  });
+});
