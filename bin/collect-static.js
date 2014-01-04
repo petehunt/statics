53b7986805746b695d984559b72c4fb1a811a040
Cheng Lou <chenglou92@gmail.com>
2014-01-04 06:01:59 -0800

    new specs trial

--- bin/collect-static.js
+++ bin/collect-static.js
@@ -1,15 +1,6 @@
 #!/usr/bin/env node
 
 var optimist = require('optimist')
-  .usage('Usage: $0 [entrypoint module] [dest dir]');
-var argv = optimist.argv;
+  .usage('Usage: $0');
 
-if (argv._.length !== 2) {
-  optimist.showHelp();
-  process.exit(1);
-}
-
-require('../src/collectStatic')(
-  argv._[0],
-  argv._[1]
-);
+require('../src/collectStatic')();
--- src/collectStatic.js
+++ src/collectStatic.js
@@ -1,119 +1,51 @@
 // Static asset collector
 
-var fs = require('fs-extra');
-var glob = require('glob').sync;
-var mdeps = require('module-deps');
+var fs = require('fs');
+var glob = require('glob');
+var mkdirp = require('mkdirp');
 var path = require('path');
-var resolve = require('resolve');
-var through = require('through');
+var rimraf = require('rimraf');
 
-var existsCache = {};
+function _getStatics(root) {
+  var files = glob.sync(path.join(root, './static/*'));
+  if (!files.length) return [];
 
-function existsSync(path) {
-  if (typeof existsCache[path] === 'undefined') {
-    existsCache[path] = fs.existsSync(path);
-  }
-  return existsCache[path];
+  return _getStatics(path.join(root, 'node_modules/react-*'))
+    .reduce(function(accum, file) {
+      return accum.concat(file);
+    }, files);
 }
 
-function getPackageJsonPath(absoluteModulePath) {
-  var candidate = absoluteModulePath;
-  var prevCandidate = null;
-  while (candidate !== prevCandidate) {
-    var packageJsonPath = path.join(candidate, 'package.json');
-    if (existsSync(packageJsonPath)) {
-      return packageJsonPath;
-    }
-    prevCandidate = candidate;
-    candidate = path.dirname(candidate);
-  }
-  return null;
+function _extractModuleNameFromPath(modulePath) {
+  var matchedIndex = modulePath.lastIndexOf('react-');
+  // is the static folder of the current component
+  if (matchedIndex === -1) return path.basename(__dirname);
+
+  var name = modulePath.slice(matchedIndex);
+  return name.slice(0, name.indexOf('/'));
 }
 
-function defaultPlugin(destDir, projectRoot, projectStaticRoot) {
-  var files = glob(path.join(projectRoot, projectStaticRoot, '**'));
-  fs.mkdirpSync(destDir);
-
-  files.forEach(function(file) {
-    var relativePath = path.dirname(
-      path.resolve(file).replace(
-        path.resolve(path.join(projectRoot, projectStaticRoot)),
-        ''
-      )
-    ).slice(1);
-
-    var destFile = path.join(destDir, relativePath, path.basename(file));
-    if (fs.existsSync(destFile)) {
-      fs.unlinkSync(destFile);
-    }
-
-    if (fs.statSync(file).isFile()) {
-      fs.mkdirpSync(path.dirname(destFile));
-      fs.symlinkSync(file, destFile);
-    }
-  });
+function _extractStaticNameFromPath(staticPath) {
+  return path.basename(staticPath);
 }
 
-function collectStatic(entrypoint, destDir, cb) {
-  var packageJsonPathsVisited = {};
+// TODO: in the end, it's possible that 2 components require react-spinner,
+// which is troublesome especially if the two versions are different. `npm
+// dedupe` won't help here; we'll see...
+function collectStatic(entryPoint, next) {
+  var destFolder = path.join(entryPoint, 'build/static');
+  rimraf.sync(destFolder);
+  mkdirp.sync(destFolder);
 
-  cb = cb || function() {};
-
-  // streams suck, or i'm tired.
-  var semaphore = 0;
-  var called = false;
-
-  var stream = through(function(data) {
-    var packageJsonPath = getPackageJsonPath(data.id);
-    if (!packageJsonPath || packageJsonPathsVisited[packageJsonPath]) {
-      return;
-    }
-    packageJsonPathsVisited[packageJsonPath] = true;
-    var packageJson = JSON.parse(fs.readFileSync(packageJsonPath, {encoding: 'utf8'}));
-
-    if (packageJson.static && packageJson.static.root) {
-      try {
-        semaphore++;
-        var pluginName = packageJson.static.plugin;
-        var gotPlugin = function(plugin) {
-          plugin(destDir, path.join(packageJsonPath, '..'), packageJson.static.root);
-          semaphore--;
-          if (semaphore === 0) {
-            called = true;
-            cb();
-          }
-        }.bind(this);
-
-        if (pluginName) {
-          resolve(pluginName, {basedir: path.join(packageJsonPath, '..')}, function(err, res) {
-            if (err) {
-              console.error(err);
-              semaphore--;
-              if (semaphore === 0) {
-                called = true;
-                cb();
-              }
-              return;
-            }
-            gotPlugin(require(res));
-          }.bind(this));
-        } else {
-          gotPlugin(defaultPlugin);
-        }
-      } catch (e) {
-        console.error(e);
-      }
-    }
-  }, function() {
-    if (semaphore === 0 && !called) {
-      cb();
-    }
+  _getStatics(entryPoint).forEach(function(staticPath) {
+    var moduleName = _extractModuleNameFromPath(staticPath);
+    var staticName = _extractStaticNameFromPath(staticPath);
+    // format: react-spinner_bar.css
+    var destPath = path.join(destFolder,  moduleName + '_' + staticName);
+    fs.symlinkSync(path.resolve(staticPath), destPath);
   });
 
-  mdeps(
-    [path.resolve(entrypoint)],
-    {transformKey: ['browserify', 'transform']}
-  ).pipe(stream);
+  next && next();
 }
 
 module.exports = collectStatic;
