6f96333323d9cadaee95eb516c23476f39f5e5ab
Cheng Lou <chenglou92@gmail.com>
2014-01-04 23:24:03 -0800

    css rules namespacing for . and #

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 // Static asset collector
 
-var fs = require('fs');
+var fs = require('fs-extra');
 var glob = require('glob');
-var mkdirp = require('mkdirp');
+var mimetype = require('mimetype');
 var path = require('path');
-var rimraf = require('rimraf');
+var rework = require('rework');
+var reworkNamespace = require('rework-namespace');
 
 function _getStatics(root) {
   var files = glob.sync(path.join(root, 'static/*'));
@@ -32,20 +33,45 @@ function _extractStaticNameFromPath(staticPath) {
   return path.basename(staticPath);
 }
 
+function _namespaceCSSUrls(src, namespace) {
+  return rework(src).use(rework.url(function(url) {
+    return namespace + '_' + url;
+  })).toString();
+}
+
+// TODO: keyframes, etc.
+function _namespaceCSSSelectors(src, namespace) {
+  return rework(src).use(reworkNamespace(namespace + '_')).toString();
+}
+
+function _rewriteCSSByNamespacing(cssPath, namespace) {
+  var src = fs.readFileSync(cssPath, {encoding: 'utf8'});
+  var newSrc = _namespaceCSSUrls(src, namespace);
+  var newSrc2 = _namespaceCSSSelectors(newSrc, namespace);
+  fs.writeFileSync(cssPath, newSrc2, {encoding: 'utf8'});
+}
+
 // TODO: in the end, it's possible that 2 components require react-spinner,
 // which is troublesome especially if the two versions are different. `npm
 // dedupe` won't help here; we'll see...
 function collectStatic(entryPoint, next) {
   var destFolder = path.join(entryPoint, 'build/static');
-  rimraf.sync(destFolder);
-  mkdirp.sync(destFolder);
+  fs.removeSync(destFolder);
+  fs.mkdirpSync(destFolder);
 
   _getStatics(entryPoint).forEach(function(staticPath) {
     var moduleName = _extractModuleNameFromPath(staticPath);
     var staticName = _extractStaticNameFromPath(staticPath);
-    // format: react-spinner_bar.css
+
     var destPath = path.join(destFolder,  moduleName + '_' + staticName);
-    fs.symlinkSync(path.resolve(staticPath), destPath);
+    if (mimetype.lookup(staticPath) === 'text/css') {
+      // format: react-spinner_bar.css
+      fs.copySync(staticPath, destPath);
+      _rewriteCSSByNamespacing(destPath, moduleName);
+    } else {
+      // img copying too expensive; symlink them
+      fs.symlinkSync(path.resolve(staticPath), destPath);
+    }
   });
 
   next && next();
